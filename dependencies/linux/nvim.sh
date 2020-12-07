d3685d6afb1eede72722cdd68b9b7d94f66bc503
Sergey Kluchkovsky <kaineer@gmail.com>
2020-12-07 13:12:24 +0500

    Refactor nvim.sh

--- dependencies/linux/nvim.sh
+++ dependencies/linux/nvim.sh
@@ -2,9 +2,11 @@
 # url: https://github.com/neovim/neovim/wiki/Installing-Neovim
 # needs: wget aliases
 
-mkdir -p ~/bin/appimages
-wget https://github.com/neovim/releases/latest/download/nvim.appimage -O ~/bin/appimages/nvim.appimage
-chmod 755 ~/bin/appimages/nvim.appimage
+NVIM_DIR="$HOME/bin/appimages"
 
-echo "alias vim=~/bin/appimages/nvim.appimage" >~/bin/aliases/nvim.sh
+mkdir -p $NVIM_DIR
+wget https://github.com/neovim/releases/latest/download/nvim.appimage -O $NVIM_DIR/nvim.appimage
+chmod 755 $NVIM_DIR/nvim.appimage
+
+echo "alias vim=$NVIM_DIR/nvim.appimage" >~/bin/aliases/nvim.sh
 chmod 755 ~/bin/aliases/nvim.sh
