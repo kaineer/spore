a7bd58b157222f953ace42be7bbb6500ed64912d
Sergey Kluchkovsky <kaineer@gmail.com>
2020-12-10 13:23:41 +0500

    Recent additions

---
+++ dependencies/debian/gh.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# url: https://github.com/cli/cli
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
---
+++ dependencies/linux/fd.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# needs: cargo
+
+sudo cargo install fd-find
