3326aed4af4660cce05c8bb56ee59eb5adac975a
Sergey Kluchkovsky <kaineer@gmail.com>
2020-12-11 13:36:43 +0500

    Moves alactitty for debian

--- dependencies/debian/alacritty.sh
+++ dependencies/debian/alacritty.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-# needs: cargo
+# needs: wget
 
-sudo cargo install alacritty
+mkdir -p "$HOME/git/tmp"
+
+wget https://github.com/alacritty/alacritty/releases/download/v0.3.3/Alacritty-v0.3.3-ubuntu_18_04_amd64.deb -O "$HOME/git/tmp/alacritty.deb"
+
+sudo dpkg -i "$HOME/git/tmp/alacritty.deb"
